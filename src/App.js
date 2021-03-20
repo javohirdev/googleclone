5cdc8545fed260a5b65325329df3df2fac85ba99
Javohir Hakimov <57003102+javohirdev@users.noreply.github.com>
2021-03-20 12:38:04 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Router, Switch, Route} from "react-router-dom";
 import Nav from './GoogleClone/homeComponents/Nav';
-import SearchPage from './GoogleClone/searchPage/SeachRes';
+import SearchRes from './GoogleClone/searchPage/SeachRes';
 import './GoogleClone/styles/design.css';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Nav} />
-          <Route path="/search" component={SearchPage} />
+          <Route path="/search" component={SearchRes} />
         </Switch>
       </BrowserRouter>
     </>
